8bfafe51a226f98fe9ad6c347ab529af518cf2a4
WillBrownsberger <WillBrownsberger@gmail.com>
2014-08-17 17:32:46 -0400

    checklist

--- 404.php
+++ 404.php
@@ -1,36 +1,29 @@
 <?php
 /*
- * Template Name: Full Width Page
- * Description: A Page Template that uses full 1280 px of view frame -- no sidebar.
+ * Template Name: 404
+ * Description: Displayed when material not found
  *
- * page.php -- note, all calls to bbpress are intercepted first by forum.php
- * 
- * http://codex.wordpress.org/Page_Templates
+ * @package responsive-tabs
  *
  */
+
+/* assure that will die if accessed directly */ 
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
+
 get_header();
-echo '<!--page.php -->';
 
-$new_link = str_replace('/forums/topic', '', $_SERVER[REQUEST_URI]); 
+?>
 
-// division wraps the non-sidebar, non-footer content
-?> 
+<!--responsive-tabs/404.php -->
 
 <div id="full-width-content-wrapper" > 
 	
-	<h1>Sorry!</h1>
+	<h1><?php _e( 'Sorry! We cannot find the content you requested.', 'responsive-tabs' )?></h1>
 	
-	<h2>We've moved some links around.<h2>
-
-	<h3>If you need help, please don't hesitate to <a href="/connect/contact">contact us directly</a>.</h3>
-
-<?php if(strpos('xxx'. $_SERVER[REQUEST_URI],'/forums/topic')>0)  { ?>  
-	<h3><a href = "<?php echo $new_link; ?>">Click here for a good guess on the correct URL</a></h3>
-	<?php } ?>
-	<h3><a href="<?php  echo( home_url( '/' ) ) ?>" title="Go to front page">Click here to return home at 
-					 <?php bloginfo( 'name' ) ?> </a>
-
+	<h3><?php printf( _e( 'Please <a href="javascript: history.go(-1)">go back</a> or start over from <a href="%1$s">front page of %2$s</a>.' ), home_url( '/' ), get_bloginfo( 'name')  ); ?></h3>
+					
 </div>
-<div class="horbar_clear_fix"></div><?php 
- 
-get_footer();
+
+
+
+<?php get_footer();
