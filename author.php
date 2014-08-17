7bd690ed59322187bbd1bf26371ad23b988bb798
WillBrownsberger <WillBrownsberger@gmail.com>
2014-08-17 18:15:36 -0400

    checklist

--- author.php
+++ author.php
@@ -12,7 +12,6 @@ defined( 'ABSPATH' ) or die( "No script kiddies please!" );
 
 get_header();
 
-
 /* set up title for author search */
 
 $curauth = ( get_query_var( 'author_name' ) ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
@@ -22,12 +21,12 @@ $curauth = ( get_query_var( 'author_name' ) ) ? get_user_by( 'slug', get_query_v
 
 <div id = "content-header">
 
-	<?php get_template_part('breadcrumbs'); ?>
+	<?php get_template_part( 'breadcrumbs' ); ?>
 	 
  	<h1><?php echo $curauth->display_name; ?> </h1>
 
-	<select name="author-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
-  		<option value=""><?php echo esc_attr( __( 'Select Author' ) ); ?></option> 
+	<select id="author-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+  		<option value=""><?php _e( 'Select Author', 'responsive-tabs' ); ?></option> 
 		<?php twcc_list_author_dropdown ($args); ?>
 	</select>
 	
@@ -35,11 +34,11 @@ $curauth = ( get_query_var( 'author_name' ) ) ? get_user_by( 'slug', get_query_v
 
 <div id = "post-list-wrapper">
  
-	<?php get_template_part('post','list'); ?>
+	<?php get_template_part( 'post', 'list' ); ?>
 	
 </div> 
 	
 <!-- empty bar to clear formatting -->
-<div class="horbar-clear-fix"></div><?php 
- 
-get_footer();
+<div class="horbar-clear-fix"></div>
+
+<?php get_footer();
