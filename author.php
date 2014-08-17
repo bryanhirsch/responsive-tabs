d6a7cc65e5b4eae9b12a96e8bda51e5df0ac7b97
WillBrownsberger <WillBrownsberger@gmail.com>
2014-08-17 18:07:15 -0400

    checklist

--- author.php
+++ author.php
@@ -1,38 +1,45 @@
 <?php
 /*
-*
-* date.php
-*
-*/
+ * Template Name: author
+ * Description: Displayed for author archive searches
+ *
+ * @package responsive-tabs
+ *
+ */
+
+/* assure that will die if accessed directly */ 
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
+
 get_header();
 
-echo '<!-- author.php -->';
+
 /* set up title for author search */
 
-
-$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
-
-
+$curauth = ( get_query_var( 'author_name' ) ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
 ?>
+
+<!-- responsive-tabs author.php -->
+
 <div id = "content-header">
-<?php get_template_part('breadcrumbs'); ?> 
- <h1><?php 
-echo $curauth->display_name; ?> </h1>
 
-<select name="author-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
-  <option value=""><?php echo esc_attr( __( 'Select Author' ) ); ?></option> 
-<?php twcc_list_author_dropdown ($args); ?>
-</select>
-</div>
-<?php  echo 	'<div id = "post-list-wrapper">';
+	<?php get_template_part('breadcrumbs'); ?>
+	 
+ 	<h1><?php echo $curauth->display_name; ?> </h1>
 
-// multiple post handler 
-get_template_part('post','list');
-echo '</div>'; 
-?> 
-
+	<select name="author-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+  		<option value=""><?php echo esc_attr( __( 'Select Author' ) ); ?></option> 
+		<?php twcc_list_author_dropdown ($args); ?>
+	</select>
 	
- <!-- empty bar to clear formatting -->
-<div class="horbar_clear_fix"></div><?php 
+</div>
+
+<div id = "post-list-wrapper">
+ 
+	<?php get_template_part('post','list'); ?>
+	
+</div> 
+	
+<!-- empty bar to clear formatting -->
+<div class="horbar-clear-fix"></div><?php 
  
 get_footer();
